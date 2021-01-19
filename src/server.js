bae0c5597be381563926bb5d77adff7945f79d4d
vpham20 <vinh.pham@dxc.com>
2021-01-19 14:52:35 +0700

    add app insight

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 8080
+let appInsights = require("applicationinsights");
+appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY || "").start();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
