81b2e04424366178cc5a7230677de9e935fa9cd6
vpham20 <vinh.pham@dxc.com>
2021-01-19 15:06:04 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 80
 let appInsights = require("applicationinsights");
 appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY || "").start();
 
