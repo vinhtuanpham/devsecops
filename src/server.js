7e3664fcdb4b7455d1d60ea59a743d4b88b529fa
vinhtuanpham <vpham20@csc.com>
2021-01-21 18:41:41 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,39 @@
+var express = require('express');
+var app = express();
 
-const express = require('express')
-const app = express()
-const port = 8080
-// let appInsights = require("applicationinsights");
-// appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY || "").start();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
 })
